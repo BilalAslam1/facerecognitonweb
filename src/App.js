929c4f013d349b62064d531fbf8a3a8e4b6e2ef9
Bilal <Bilal.aslam.sheikh@gmail.com>
2020-07-23 19:10:00 -0400

    added image link form and particle js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Navigation from "./Components/Navigation/Navigation.js";
+import Logo from "./Components/Logo/Logo.js";
+import Particles from "react-particles-js";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
+import Rank from "./Components/Rank/Rank.js";
 import "./App.css";
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 140,
+    },
+    density: {
+      enable: true,
+      value_area: 1000,
+    },
+    size: {
+      value: 3,
+    },
+  },
+};
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptions} />
       <Navigation />
-      {/* { <Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
+      {/* {
       <FaceRecognition /> } */}
     </div>
   );
